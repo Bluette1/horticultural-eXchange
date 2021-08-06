0a2c6310c481914548fed5d203d909db3d8f8ef9
Bluette1 <marylene.sawyer@gmail.com>
2021-08-06 12:50:36 +0200

    Add integration with Stripe

---
+++ src/services/payment.service.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+import { httpProtocol, host, port } from '../env.variables';
+
+const API_URL = `${httpProtocol}://${host}:${port}/payment_intents/`;
+
+const createPaymentIntent = (items) => {
+  return axios.post(API_URL,
+    { payment_intent: items },
+    { headers: authHeader() },
+  );
+};
+
+export default createPaymentIntent;
