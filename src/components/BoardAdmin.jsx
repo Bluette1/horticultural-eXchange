4749cac583ea37772caf9ab89b8bebfb5e265968
Bluette1 <marylene.sawyer@gmail.com>
2021-08-22 10:23:02 +0200

    Add styling links - admin and moderator board issue #3

--- src/components/BoardAdmin.jsx
+++ src/components/BoardAdmin.jsx
@@ -1,16 +1,57 @@
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const BoardAdmin = () => (
-  <div className="container">
-    <header className="jumbotron">
-      <h3>Admin Board</h3>
-    </header>
-    <div className="d-flex flex-column">
-      <Link to="/new-plant">Add a product item</Link>
-      <Link to="/new-category">Add a product category</Link>
-      <Link to="/update-product/select">Update an existing product</Link>
+const BoardAdmin = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
+
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+  if (
+    currentUser
+    && !currentUser.supervisor_role
+    && !currentUser.superadmin_role
+  ) {
+    alert(
+      'Unauthorized action! You need to be logged in as admin or supervisor.',
+    );
+    return <Redirect to="/profile" />;
+  }
+  return (
+    <div className="container d-flex justify-content-center ">
+      <div className="col-md-6">
+        <header className="jumbotron">
+          <h3
+            style={{
+              textTransform: 'uppercase',
+              paddingBottom: '50px',
+              paddingTop: '50px',
+              textAlign: 'center',
+            }}
+          >
+            Admin Board
+          </h3>
+        </header>
+        <div className="d-flex flex-column admin">
+          <span className="d-flex justify-content-between">
+            <i className="fa fa-plus-circle" aria-hidden="true" />
+            <Link to="/new-plant">Add a product item</Link>
+            <i className="fa fa-leaf" aria-hidden="true" />
+          </span>
+          <span className="d-flex justify-content-between">
+            <i className="fa fa-plus-circle" aria-hidden="true" />
+            <Link to="/new-category">Add a product category</Link>
+            <i className="fa fa-object-group" aria-hidden="true" />
+          </span>
+          <span className="d-flex justify-content-between">
+            <i className="fa fa-plus-circle" aria-hidden="true" />
+            <Link to="/update-product/select">Update an existing product</Link>
+            <i className="fa fa-leaf" aria-hidden="true" />
+          </span>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BoardAdmin;
--- src/components/BoardModerator.jsx
+++ src/components/BoardModerator.jsx
@@ -1,15 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const BoardModerator = () => (
-  <div className="container">
-    <header className="jumbotron">
-      <h3>Moderator Board</h3>
-    </header>
-    <div className="d-flex flex-column">
-      <Link to="/register">Add User</Link>
-      <Link to="/deregister">Remove User</Link>
+const BoardModerator = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+  if (currentUser && !currentUser.supervisor_role && !currentUser.superadmin_role) {
+    alert('Unauthorized action! You need to be logged in as admin or supervisor.');
+    return <Redirect to="/profile" />;
+  }
+  return (
+    <div className="pt-5 container d-flex justify-content-center align-items-center ">
+      <div>
+        <header className="jumbotron">
+          <h3 style={{ textTransform: 'uppercase', paddingBottom: '30px' }}>Moderator Board</h3>
+        </header>
+        <div className="d-flex flex-column mod">
+          <span className="d-flex justify-content-between">
+            <i className="fa fa-plus-circle" aria-hidden="true" />
+            <Link to="/register">Add User</Link>
+            <i className="fa fa-user-circle-o" aria-hidden="true" />
+          </span>
+          <span className="d-flex justify-content-between">
+            <i className="fa fa-minus-circle" aria-hidden="true" />
+            <Link to="/deregister">Remove User</Link>
+            <i className="fa fa-user-circle-o" aria-hidden="true" />
+          </span>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BoardModerator;
