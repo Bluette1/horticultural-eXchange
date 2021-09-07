7a76d5b129d80edad2d58c4ee1abbd8e7a96f81f
Bluette1 <marylene.sawyer@gmail.com>
2021-09-07 10:51:45 +0200

    Add more tests - App
    issue #2

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {
   render, screen, cleanup, waitFor,
 } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import { Provider } from 'react-redux';
 import axios from 'axios';
-import App from './App';
-import store from './store';
-import { httpProtocol, host, port } from './env.variables';
+import App from '../App';
+import store from '../store';
+import { httpProtocol, host, port } from '../env.variables';
 
 jest.mock('axios');
 
@@ -32,8 +33,26 @@ test('renders the app', async () => {
   });
   const app = render(<AppWithStore />);
   await waitFor(() => {
+    const navbar = document.querySelector('.navbar');
+    expect(navbar).toBeTruthy();
+
+    const navItems = document.querySelectorAll('.nav-item');
+    expect(navItems.length).toBe(3);
+    
+    const login = screen.getByText(/login/i);
+    expect(login).toBeInTheDocument();
+
+    const signUp = screen.getByText(/sign up/i);
+    expect(signUp).toBeInTheDocument();
+
+    const productCategories = screen.getAllByText(/product categories/i);
+    expect(productCategories.length).toBe(2);
+
     const iGrowTitle = screen.getByText(/iGrow/i);
-    expect(iGrowTitle).toBeInTheDocument;
+    expect(iGrowTitle).toBeInTheDocument();
+    const img = screen.queryByAltText('logo');
+    expect(img).toHaveAttribute('src', 'logo.png');
+
     expect(app).toMatchSnapshot();
   });
 });
