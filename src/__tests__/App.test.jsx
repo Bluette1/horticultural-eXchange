19bfbbf627e990883c96bc783ff30874bd7abd18
Bluette1 <marylene.sawyer@gmail.com>
2021-09-22 06:07:07 +0200

    Update `App.test`
    issue #2

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {
-  render, screen, cleanup, waitFor,
+  render, within, cleanup, waitFor,
 } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 import App from '../App';
 import store from '../store';
 import { httpProtocol, host, port } from '../env.variables';
+import Login from '../components/Login';
 
 jest.mock('axios');
 
@@ -20,7 +21,7 @@ const AppWithStore = () => (
     </React.StrictMode>
   </Provider>
 );
-test('renders the app', async () => {
+test('renders the app - navbar links are displayed correctly', async () => {
   axios.get.mockImplementation((url) => {
     switch (url) {
       case `${httpProtocol}://${host}:${port}/categories`:
@@ -31,28 +32,34 @@ test('renders the app', async () => {
         return Promise.resolve({ data: [] });
     }
   });
-  const app = render(<AppWithStore />);
+  const { container } = render(<AppWithStore />);
   await waitFor(() => {
-    const navbar = document.querySelector('.navbar');
-    expect(navbar).toBeTruthy();
+    // const navBar = container.querySelector('.navbar');
+    const navBar = container.firstChild.firstChild;
+    expect(navBar).toHaveClass('navbar navbar-expand-lg navbar-dark bg-navbar d-flex justify-content-around justify-content-lg-between');
+    const logoImg = within(navBar).getByAltText("logo");
+    expect(logoImg).toBeInTheDocument();
+    expect(logoImg).toHaveAttribute('src', 'logo.png');
+    expect(within(navBar).getByText(/iGrow/)).toBeInTheDocument();
 
-    const navItems = document.querySelectorAll('.nav-item');
-    expect(navItems.length).toBe(3);
+    expect(navBar).toBeTruthy();
+    expect(navBar.children).toHaveLength(2);
+
+    expect(container).toMatchSnapshot();
+
+    const navItems = navBar.querySelectorAll('.nav-item');
+    expect(navItems).toHaveLength(3);
     
-    const login = screen.getByText(/login/i);
-    expect(login).toBeInTheDocument();
+    const home = navItems[0].firstChild;
+    expect(within(home).getByText("Home")).toBeInTheDocument();
+    expect(home).toContainHTML('<a class="nav-link" href="/home">Home</a>');
 
-    const signUp = screen.getByText(/sign up/i);
-    expect(signUp).toBeInTheDocument();
+    const login = navItems[1].firstChild;
+    expect(within(login).getByText("Login")).toBeInTheDocument();
+    expect(login).toContainHTML('<a class="nav-link" href="/login">Login</a>');
 
-    const productCategories = screen.getAllByText(/product categories/i);
-    expect(productCategories.length).toBe(2);
-
-    const iGrowTitle = screen.getByText(/iGrow/i);
-    expect(iGrowTitle).toBeInTheDocument();
-    const img = screen.queryByAltText('logo');
-    expect(img).toHaveAttribute('src', 'logo.png');
-
-    expect(app).toMatchSnapshot();
+    const signup = navItems[2].firstChild;
+    expect(within(signup).getByText("Sign Up")).toBeInTheDocument();
+    expect(signup).toContainHTML('<a class="nav-link" href="/register">Sign Up</a>');
   });
 });
